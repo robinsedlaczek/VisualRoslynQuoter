2baf2c0752bacd1da2e4373088d1773c35882300
robin:sedlaczek <robin.sedlaczek@live.de>
2016-02-08 07:41:32 +0100

    Some optimization (quote code only if the text view has changed).

---
+++ WaveDev.VisualRoslynQuoter/WaveDev.VisualRoslynQuoter/VisualCodeQuoterToolWindowControl.xaml.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="VisualCodeQuoterToolWindowControl.xaml.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace WaveDev.VisualRoslynQuoter
+{
+    using Microsoft.VisualStudio.Text.Editor;
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for VisualCodeQuoterToolWindowControl.
+    /// </summary>
+    public partial class VisualCodeQuoterToolWindowControl : UserControl
+    {
+        public string OldCode{ get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VisualCodeQuoterToolWindowControl"/> class.
+        /// </summary>
+        public VisualCodeQuoterToolWindowControl()
+        {
+            this.InitializeComponent();
+
+            WpfTextViewCreationListener.TextViewLayoutChanged += OnWpfTextViewCreationListenerTextViewLayoutChanged;
+        }
+
+        private void OnWpfTextViewCreationListenerTextViewLayoutChanged(TextViewLayoutChangedEventArgs e)
+        {
+            try
+            {
+                var code = e.NewSnapshot.GetText();
+
+                if (code == OldCode)
+                    return;
+
+                var quoter = new Quoter();
+                var quotedCode = quoter.Quote(code);
+
+                QuotedCodeTextBox.Text = quotedCode;
+                OldCode = code;
+            }
+            catch (Exception exception)
+            {
+
+            }
+        }
+    }
+}
